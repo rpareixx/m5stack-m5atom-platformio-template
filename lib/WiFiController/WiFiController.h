577a83ef1b9c6e2d872577a5d16477f920b13557
Robert Parei <78567748+rpareixx@users.noreply.github.com>
2025-01-08 13:38:44 +0100

    Initial commit

---
+++ lib/WiFiController/WiFiController.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <WiFi.h>
+
+#include "NVSLoader.h"
+
+class WiFiController {
+public:
+    static const int8_t WIFI_RETRY = 30;
+    static const unsigned long WIFI_RECONNECT_INTERVAL_MS = 30000;
+
+    WiFiController(const char* nvsNamespace);
+    virtual ~WiFiController(void);
+
+    virtual bool begin(const char* nvsConfigPath,
+                       void (*connectingCallback)(uint8_t retries));
+    virtual bool update(void);
+    virtual bool isConnected(void) const;
+    virtual bool disconnect(void);
+
+    virtual bool getPreference(const char* key, char* buf, size_t size);
+
+    virtual WiFiClient& getClient(void);
+
+protected:
+    virtual bool connectWiFi(const char* ssid, const char* passwd,
+                             uint8_t retries,
+                             void (*connectingCallback)(uint8_t retries));
+
+private:
+    WiFiClient client;
+    NVSLoader nvsLoader;
+    unsigned long prevConnTime;
+};
